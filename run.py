8255b098bdbd70bf04267d6d8622d81888e951ca
impshum <impshum@impshum.co.uk>
2018-06-21 11:31:37 +0100

    random screenshot name

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 import pyscreenshot
+import random
+import string
 
 
 app = Flask(__name__)
@@ -12,8 +14,10 @@ def index():
 @app.route('/get_screenshot', methods=['POST'])
 def get_screenshot():
     im = pyscreenshot.grab()
-    im.save('static/img/ss.png')
-    return render_template('show.html', screenshot='/static/img/ss.png')
+    random_id = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)])
+    file_name = 'static/img/{}.png'.format(random_id)
+    im.save(file_name)
+    return render_template('show.html', screenshot=file_name)
 
 
 if __name__ == '__main__':
